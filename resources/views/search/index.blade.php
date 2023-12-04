23e6ea516b0f608395816bcd54ba0f14b216d88d
Ferdinand Martirez <fmartirez15@gmail.com>
2023-12-04 15:45:21 +0800

    added Title

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
         crossorigin="anonymous">
-    <title>Information</title>
+    <title>AwesomeTravelHub</title>
     <style>
         body {
             padding: 20px;
@@ -101,6 +101,7 @@
 
 <body>
     <div class="container">
+        <h1 class="text-center">AwesomeTravelHub</h1>
         <form action="/" method="GET">
              <div class="mb-3">
                 <label for="city" class="form-label">City:</label>
