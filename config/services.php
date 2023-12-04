b1940fed458964a91ee968cd9a52291aa6f79a28
Ferdinand Martirez <fmartirez15@gmail.com>
2023-12-04 15:21:52 +0800

    update services

--- config/services.php
+++ config/services.php
@@ -31,4 +31,13 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'openweather' => [
+        'key' => env('OPENWEATHER_API_KEY'),
+        'url' => env('OPENWEATHER_API_URL')
+    ],
+
+    'foursquare' => [
+        'key' => env('FOURSQUARE_API_KEY'),
+        'url' => env('FOURSQUARE_API_URL')
+    ],
 ];
